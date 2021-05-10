1a9604461a89ba174d24a41417aa6e0533816fc4
raviranchod <raviranchod@gmail.com>
2021-05-10 23:19:27 +0100

    feat: configured postgresql database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
