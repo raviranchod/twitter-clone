78686ba846f924595dbdd781401bc00058654536
raviranchod <raviranchod@gmail.com>
2021-05-12 17:23:52 +0100

    feat: heading component

---
+++ client/src/components/Heading.tsx
@@ -0,0 +1,12 @@
+type HeadingProps = {
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Heading = ({ as = "h1", children, className }: HeadingProps) => {
+  const H = as;
+  return <H className={className}>{children}</H>;
+};
+
+export { Heading };
