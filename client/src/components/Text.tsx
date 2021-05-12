8dd4c6a5b413e3ead789b12c418b53ecb979ca82
raviranchod <raviranchod@gmail.com>
2021-05-12 17:24:04 +0100

    feat: text component

---
+++ client/src/components/Text.tsx
@@ -0,0 +1,12 @@
+type TextProps = {
+  as?: "p" | "span";
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Text = ({ as = "p", children, className }: TextProps) => {
+  const T = as;
+  return <T className={className}>{children}</T>;
+};
+
+export { Text };
