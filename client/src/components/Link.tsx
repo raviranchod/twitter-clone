aea21fab3c3afc8968278a1bc713950c9f785b22
raviranchod <raviranchod@gmail.com>
2021-05-12 18:08:26 +0100

    feat: link component

---
+++ client/src/components/Link.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NextLink from "next/link";
+
+type LinkProps = {
+  children: React.ReactNode;
+  className?: string;
+  href: string;
+};
+
+const Link = ({ children, className, href }: LinkProps) => (
+  <NextLink href={href}>
+    <a className={className}>{children}</a>
+  </NextLink>
+);
+
+export { Link };
