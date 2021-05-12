8b63ecaa15131909e2460ddbf0704422dde525b5
raviranchod <raviranchod@gmail.com>
2021-05-12 17:25:32 +0100

    refactor: add theme colors and base styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
+    colors: {
+      // base colours
+      black: "#000000",
+      white: "#ffffff",
+      current: "currentColor",
+      transparent: "transparent",
+
+      //
+      primary: "#1da1f2",
+      cloudy: "#ebf6fe",
+    },
     extend: {},
   },
   variants: {
