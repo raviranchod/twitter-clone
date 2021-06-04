975c472a665278822afe6cd8efa7a06ad42b849a
raviranchod <raviranchod@gmail.com>
2021-06-04 18:22:31 +0100

    feat: profile image component

---
+++ client/src/components/ProfileImage.tsx
@@ -0,0 +1,29 @@
+type ProfileImageProps = {
+  alt: string;
+  className?: string;
+  size?: "sm" | "md" | "lg";
+  src: string;
+};
+
+const ProfileImage = ({
+  alt,
+  className,
+  src,
+  size = "md",
+}: ProfileImageProps) => {
+  const sizeClassName = {
+    sm: "w-10",
+    md: "w-14",
+    lg: "w-28",
+  };
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={`rounded-lg ${sizeClassName[size]} ${className}`}
+    />
+  );
+};
+
+export { ProfileImage };
