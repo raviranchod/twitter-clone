28a65fc6352a76e9b272e5f9585fe60e19b03bfb
raviranchod <raviranchod@gmail.com>
2021-06-16 16:33:26 +0100

    refactor: panel layout logo display

--- client/src/layouts/PanelLayout.tsx
+++ client/src/layouts/PanelLayout.tsx
@@ -11,8 +11,8 @@ const PanelLayout = ({ children }: PanelLayout) => (
     <div className="hidden sm:flex"></div>
     <div className="max-w-md w-full mx-auto">
       <FormPanel>
-        <Link href="/" className="block">
-          <Logo className="mb-4" />
+        <Link href="/" className="inline-block mb-4">
+          <Logo />
         </Link>
         {children}
       </FormPanel>
