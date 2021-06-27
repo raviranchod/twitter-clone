bd08ba587df92167d45edadbad05220379c2f82a
raviranchod <raviranchod@gmail.com>
2021-06-27 10:42:15 +0100

    fix: testing implementation

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
   setupFilesAfterEnv: ["./setupTests.js"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  preset: "ts-jest",
 };
